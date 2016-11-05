9c47ce84664de82bd3cac2027095f62964e2d140
Resta <resta190498@gmail.com>
2016-11-05 15:15:59 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
@@ -49,6 +49,22 @@ public class InputActivity extends AppCompatActivity {
                         doSave();
                     }
                 });
+        hotel = (Hotel) getIntent().getSerializableExtra(MainActivity.HOTEL);
+        if (hotel != null) {
+            setTitle("Edit " + hotel.judul);
+            fillData();
+        } else {
+            setTitle("New Hotel");
+        }
+    }
+
+    private void fillData() {
+        etJudul.setText(hotel.judul);
+        etDeskripsi.setText(hotel.deskripsi);
+        etDetail.setText(hotel.detail);
+        etLokasi.setText(hotel.detail);
+        uriFoto = Uri.parse(hotel.foto);
+        ivFoto.setImageURI(uriFoto);
     }
 
     private void doSave() {
